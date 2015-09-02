ed27b1715717201494e940c34705a01d36cba447
Kyle Mitchell <kyle@kemitchell.com>
2015-09-02 16:00:54 -0700

    Remove extra string concatenation logic
    All non-string content objects are now passed through as objects.
    There's no longer need to check whether resolution created contiguous
    strings in a content array.

--- form.js
+++ form.js
@@ -2,7 +2,6 @@ var resolveElement = require('./element')
 
 module.exports = function(form, values, numberings, headings) {
   form.content = form.content
-    // resolve content
     .map(function(element, index) {
       var numbering = (
         ( numberings &&
@@ -10,20 +9,5 @@ module.exports = function(form, values, numberings, headings) {
           numberings.content.hasOwnProperty(index) ) ?
         numberings.content[index] : null )
       return resolveElement(element, values, numbering, headings) })
-
-    // Concatenate contiguous strings.
-    .reduce(
-      function(content, element, index) {
-        var count = content.length
-        var last = content[count - 1]
-        if (
-          index > 0 &&
-          typeof element === 'string' &&
-          typeof last === 'string')
-        { content[count - 1] = last + element }
-        else {
-          content.push(element) }
-        return content },
-      [])
 
   return form }
